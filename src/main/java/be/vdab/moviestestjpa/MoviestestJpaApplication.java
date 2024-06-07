3572e0fb1d7bc2d58578dc0bc5d68e14f0b9ff7a
Adam Hillo <adam1hillo@gmail.com>
2024-06-07 10:08:20 +0200

    Initial commit

---
+++ src/main/java/be/vdab/moviestestjpa/MoviestestJpaApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.moviestestjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviestestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviestestJpaApplication.class, args);
+	}
+
+}
